be7bafff038cf8291ee9f41f724aa6309669a4a3
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-03-24 21:59:46 +0100

    added client project shell

---
+++ src/client/diagraph-dashboard/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { GlucoseGraph } from 'modules/graph';
+
+function App() {
+  return (
+      <GlucoseGraph />
+  );
+}
+
+export default App;
---
+++ src/client/diagraph-dashboard/src/modules/graph/components/GlucoseGraph.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function GlucoseGraph() {
+    return <>Hello from the glucose graph</>
+}
