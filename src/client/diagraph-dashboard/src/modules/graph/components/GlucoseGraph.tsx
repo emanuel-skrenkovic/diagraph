d5e4015bbb788d1e6f69987ce0a86c77e4efd219
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-03-26 22:50:45 +0100

    added basis for drawing graphs

--- src/client/diagraph-dashboard/src/modules/graph/components/GlucoseGraph.tsx
+++ src/client/diagraph-dashboard/src/modules/graph/components/GlucoseGraph.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 import { GlucoseMeasurement } from 'types';
 
 import "App.css";
@@ -8,5 +9,29 @@ export interface GlucoseGraphProps {
 }
 
 export const GlucoseGraph :React.FC<GlucoseGraphProps> = ({ points }) => {
-    return <div className="item">Hello from the glucose graph!</div>
+    const curveElemRef = useRef(null);
+    const data: [number, number][] = [[0, 20], [150, 150], [300, 100], [450, 20], [600, 130]];
+
+    useEffect(() => {
+        const svg = d3.select(curveElemRef.current).append("svg").attr("width", 800).attr("height", 200);
+
+        // prepare a helper function
+        const curveFunc = d3.line()
+            .curve(d3.curveLinear)              // This is where you define the type of curve. Try curveStep for instance.
+            .x(d => d[0])
+            .y(d => d[1]);
+
+        // Add the path using this helper function
+        svg.append('path')
+            .attr('d', curveFunc(data))
+            .attr('stroke', 'black')
+            .attr('fill', 'none');
+    }, []);
+
+    return (
+        <div className="item">
+            <h2>Glucose graph</h2>
+            <div ref={curveElemRef} />
+        </div>
+    );
 }
