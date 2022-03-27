9d9a18ea5c3179ceaf66f01159590e4d11ef67ad
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-03-27 21:21:40 +0200

    graph axis labels and alignment

--- src/client/diagraph-dashboard/src/modules/graph/components/GlucoseGraph.tsx
+++ src/client/diagraph-dashboard/src/modules/graph/components/GlucoseGraph.tsx
@@ -10,6 +10,11 @@ export interface GlucoseGraphProps {
     points: GlucoseMeasurement[];
 }
 
+const MARGIN = { top: 20, bottom: 20, left: 20, right: 20 };
+const PADDING = 5;
+const WIDTH = 750 - MARGIN.left - MARGIN.right;
+const HEIGHT = 300 - MARGIN.top - MARGIN.bottom;
+
 export const GlucoseGraph :React.FC<GlucoseGraphProps> = ({ from, to, points }) => {
     const chartElemRef = useRef<HTMLDivElement>(null);
 
@@ -26,28 +31,35 @@ export const GlucoseGraph :React.FC<GlucoseGraphProps> = ({ from, to, points }) 
 
         const svg = d3.select(chartElemRef.current)
             .append("svg")
-            .attr("width", 720)
-            .attr("height", 200);
+            .attr("width", WIDTH + MARGIN.left + MARGIN.right + PADDING)
+            .attr("height", HEIGHT + MARGIN.top + MARGIN.bottom + PADDING);
 
         const x = d3.scaleTime()
             .domain([from, to])
-            .range([600, 0]);
+            .range([0, WIDTH - 2 * PADDING - MARGIN.top - MARGIN.bottom]);
         svg.append("g")
-            .attr("transform", "translate(0," + 200 + ")")
-            .call(d3.axisBottom(x))
-            .text('Time');
+            .attr("transform", `translate(${MARGIN.left + PADDING}, ${HEIGHT + 2 * PADDING})`)
+            .call(d3.axisBottom(x).ticks(12));
 
-        const formatPercent = d3.format("0");
+        svg.append("text")
+            .attr("transform", `translate(${WIDTH / 2}, ${HEIGHT + MARGIN.top + MARGIN.bottom + PADDING})`)
+            .style("text-anchor", "middle")
+            .text("Date");
+
+        const yFormat = d3.format("1");
 
         const y = d3.scaleLinear()
             .domain([0, 15])
-            .range([150, 0]);
+            .range([HEIGHT - 2 * PADDING - MARGIN.top - MARGIN.bottom, 0]);
 
         svg.append("g")
-            .call(d3.axisLeft(y).tickFormat(formatPercent));
+            .attr('class', 'y axis')
+            .attr('transform', `translate (${MARGIN.left + PADDING}, ${PADDING * 2 + MARGIN.top + MARGIN.bottom})`)
+            .call(d3.axisLeft(y).ticks(15).tickFormat(yFormat));
 
         svg.append("path")
             .datum(pointData)
+            .attr('transform', `translate(${PADDING * 3 + MARGIN.left + MARGIN.right}, ${PADDING * 2 + MARGIN.bottom + MARGIN.top})`)
             .attr("fill", "none")
             .attr("stroke", "#69b3a2")
             .attr("stroke-width", 1.2)
@@ -55,15 +67,19 @@ export const GlucoseGraph :React.FC<GlucoseGraphProps> = ({ from, to, points }) 
                 .x(d => x(d[0]))
                 .y((d) => y(d[1]))
             );
-        // Add the points
+
         svg.append("g")
             .selectAll("dot")
             .data(pointData)
             .join("circle")
+            .attr('transform', `translate(${PADDING * 3 + MARGIN.left + MARGIN.right}, ${PADDING * 2 + MARGIN.bottom + MARGIN.top})`)
             .attr("cx", d => x(d[0]))
             .attr("cy", d => y(d[1]))
-            .attr("r", 1);
-            //.attr("fill", "#69b3a2")
+            .attr("r", 2.5)
+            .on('mouseover', (d, i) => {
+                // TODO
+            });
+
     }, [from, to]);
 
     return (
